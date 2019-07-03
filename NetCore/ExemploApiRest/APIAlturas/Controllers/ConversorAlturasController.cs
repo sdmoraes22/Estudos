5e8073e558af0c5ac12e1f49763a20a9d51c332b
Cristiano Moraes Oliveira <cmoraes.ti@gmail.com>
2019-07-03 16:42:32 -0300

    Commit para estudo de projeto inicial em dotnet core e vs code com Linux

---
+++ NetCore/ExemploApiRest/APIAlturas/Controllers/ConversorAlturasController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Alturas;
+
+namespace APIAlturas.Controllers
+{
+    [Route("api/[controller]")]
+    public class ConversorAlturasController : Controller
+    {
+        // GET api/values
+        [HttpGet("PesMetros/{pes}")]
+        public object Get(double pes)
+        {
+            return new
+            {
+                alturaPes = pes,
+                alturaMetros = ConversorAlturas.PesParametros(pes)
+
+            };
+        }
+    }
+}
---
+++ NetCore/ExemploApiRest/Alturas/ConversorAlturas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Alturas
+{
+    public class ConversorAlturas
+    {
+        public static double PesParametros(double altura)
+        {
+            return Math.Round(altura * 0.3048);
+        }
+    }
+}
